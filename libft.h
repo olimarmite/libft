4b0c2d737c1d03a911bb057190f4b1d0966f6741
Olivier MARTIN <olimarti@e2r10p6.clusters.42paris.fr>
2022-11-16 22:37:04 +0100

    add ft_strtrim

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: olimarti <olimarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:01:37 by olimarti          #+#    #+#             */
-/*   Updated: 2022/11/16 16:35:52 by olimarti         ###   ########.fr       */
+/*   Updated: 2022/11/16 22:25:35 by olimarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
