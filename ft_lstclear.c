9c34b61141d03488451ddaeb49b664f2f15700c6
pasdesushi <titolivier@gmx.fr>
2022-11-20 16:27:38 +0100

    finish lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,12 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olivier <olimarti@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 16:26:44 by olivier           #+#    #+#             */
+/*   Updated: 2022/11/20 16:26:45 by olivier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+#include <stdlib.h>
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list *next;
 	if (lst == NULL || *lst == NULL)
-		return (NULL);
+		return ;
 	while ((*lst)->next != NULL)
 	{
-		lst = (*lst)->next;
+		next = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = next;
 	}
-	return (lst);
+	del((*lst)->content);
+	free(*lst);
+	*lst = 0;
 }
